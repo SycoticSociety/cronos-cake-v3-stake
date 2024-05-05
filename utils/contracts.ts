0d94ecdee3ff279d79b173cd83236d27a655e94f
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-05-05 13:11:13 -0400

    Update contracts.ts

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -3,9 +3,9 @@ import { client } from "@/app/client";
 import { getContract } from "thirdweb";
 import { stakingABI } from "./stakingABI";
 
-const nftContractAddress = "<contract_address>";
-const rewardTokenContractAddress = "<contract_address>";
-const stakingContractAddress = "<contract_address>";
+const nftContractAddress = "0x94cc5A4A392C72FD2Bd1EEaF0A220ac74F2D0A06";
+const rewardTokenContractAddress = "0xf642f88D6a0Bf75E252D532D58c857aFc3A14432";
+const stakingContractAddress = "0x52AeE68f76831145fda5ebf64DfA54326C796E4C";
 
 export const NFT_CONTRACT = getContract({
     client: client,
